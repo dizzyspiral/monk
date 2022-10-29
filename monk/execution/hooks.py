b964377a10c454a2e8da8d2841e3cec92ce9b833
dizzyspiral <dizzyspi@gmail.com>
2022-10-29 18:17:22 +0000

    hooks as classes

--- monk/execution/hooks.py
+++ monk/execution/hooks.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 
 from monk.forensics.linux import get_user_regs, get_kernel_regs, get_proc_name
 from monk.memory.memreader import get_reg
@@ -13,35 +14,108 @@ from monk.symbols.uregs.arm import *
 class MonkHookError(Exception):
     pass
 
-def remove(cb):
-    remove_callback(cb)
 
-def on_process_execute(proc_name, callback):
+class MonkCallbackError(Exception):
+    pass
 
-    def _on_switch_cb():
 
-        # There's gotta be a better way to make callable objects with bound vars on the fly...
-        def _on_proc_exec_wrapper():
-            callback()
-            remove_callback(bp)    
+class Callback:
+    def __init__(self, cb_func=None):
+        """
+        Create a new callback.
 
-        next_thread = get_reg('r2')
+        :param function cb_func: callback function
+        :param dict bind: dictionary of bound values
+        """
+        # Hooks can be installed/uninstalled by any thread. It's conceivable that they'll be
+        # manipulated both by the main thread and callback threads. So, we provide a lock.
+        # This lock _should not_ be used by the subclasses, in order to ensure that nobody
+        # does anything stupid - i.e. none of the locked operations depend on something
+        # happening in a particular thread other than the current one.
+        self._hook_lock = threading.Lock()
+        self._hooks = []
 
-        if get_proc_name(thread=next_thread) == proc_name:
-            # TODO: Figure out if the next process is a user process or kernel process, then get the
-            # saved registers
-            kernel_regs = get_kernel_regs(thread=next_thread)
-            pc = kernel_regs.pc
+        if cb_func:
+            self.run = cb_func
 
-#            if user_process:
-#                user_regs = get_user_regs(sp=kernel_regs.sp)
-#                pc = user_regs[UREGS_PC]
+    def add_hook(self, symbol, cb):
+        self._hook_lock.acquire()
+        h = _on_execute(symbol, cb)
+        self._hooks.append(h)
+        self._hook_lock.release()
 
-            bp = break_on_execute(pc, _on_proc_exec_wrapper)
+        return h  # So that hooks can be tracked and later removed individually
 
-    return on_execute("__switch_to", _on_switch_cb)
+    def remove_hook(self, hook):
+        self._hook_lock.acquire()
+        
+        try:
+            remove_callback(hook)
+        except MonkControlError as e:
+            raise MonkCallbackError("Unable to remove hook, hook did not exist") from e
 
-def on_execute(symbol, callback):
+        try:
+            self._hooks.remove(hook)
+        except ValueError:
+            pass  # hook wasn't in list, probably not a big deal if removing it from the system succeeded
+
+        self._hook_lock.release()
+
+    def run(self):
+        """
+        Run the callback. This function is overriden in the constructor by the
+        user-defined cb_func, or defined by a subclass
+        """
+        raise MonkCallbackError("Callback not initialized")
+
+    def install(self):
+        """
+        Install the callback. This function is defined by the subclass, often hooking execution
+        of a particular address or symbol.
+        """
+        raise MonkCallbackError("Callback install() not initialized")
+
+    def uninstall(self):
+        for hook in self._hooks:
+            self.remove_hook(hook)
+
+        self._hook_lock.acquire()
+        self._hooks = []
+        self._hook_lock.release()
+
+
+class OnExecute(Callback):
+    def __init__(self, symbol, cb=None):
+        super().__init__(cb)
+        self._symbol = symbol
+        self.install()
+
+    def install(self):
+        self.add_hook(self._symbol, self.run)
+
+
+class OnProcessExecute(Callback):
+    def __init__(self, proc_name, cb=None):
+        super().__init__(cb)
+        self._proc_name = proc_name
+        self.install()
+
+    def _on_switch_to(self):
+        next_thread = memreader.get_reg('r2')
+
+        if forensics.linux.get_proc_name(next_thread) == self._proc_name:
+            saved_regs = forensics.linux.get_kernel_regs()
+            self._cb_proc_exec = self.add_hook(saved_regs.pc, self._on_proc_exec)
+
+    def _on_proc_exec(self):
+        self.remove_hook(self._cb_proc_exec)
+        self.run()
+
+    def install(self):
+        self._cb_switch_to = self.add_hook("__switch_to", self._on_switch_to)
+
+
+def _on_execute(symbol, callback):
     logging.getLogger(__name__).debug("on_execute")
     if isinstance(symbol, str):
         logging.getLogger(__name__).debug("looking up symbol '%s'" % symbol)
--- rsp_example.py
+++ rsp_example.py
@@ -9,22 +9,11 @@ import monk.memory.memreader as memreader
 import monk.forensics as forensics
 import monk.execution.control as control
 import monk.execution.hooks as hooks
+from monk.symbols.uregs.arm import *
 
 
 _shutdown_flag = False
 
-
-def do_stuff():
-    control.run()
-    time.sleep(1)
-    control.stop()
-    print(memreader.get_reg('r0'))
-    print(forensics.linux.get_proc_name())
-
-def cb_hello():
-    global _shutdown_flag
-    print("Hello from callback")
-    _shutdown_flag = True
 
 count = 0
 def cb_print_next_proc():
@@ -37,32 +26,15 @@ def cb_print_next_proc():
     print("next proc = %s" % next_name)
     print()
 
-def do_basic_hook():
-    control.stop()
-    hooks.on_execute("__switch_to", cb_hello)
-    control.run()
-
-    while not _shutdown_flag:
-        time.sleep(1)
-
-    control.shutdown()
-
-def do_switch_to_hook():
-    global count
-    control.stop()
-    hooks.on_execute("__switch_to", cb_print_next_proc)
-    control.run()
-
-    while count < 3:
-        time.sleep(1)
-
-    control.shutdown()
-
-def do_task_walk():
-    control.stop()
-    forensics.linux.walk_tasks()
-    control.run()
-    control.shutdown()
+def hello():
+    print("Hello from callback")
 
 if __name__ == '__main__':
-    do_switch_to_hook()
+    h = hooks.OnExecute('__switch_to', cb_print_next_proc)
+    control.run()
+
+    while count < 5:
+        time.sleep(5)
+
+    h.uninstall()
+    control.shutdown()
